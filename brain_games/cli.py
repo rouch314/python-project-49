aa7353e05e2261c1ab0cf91a0ebfb5fd08cba5fd
Sergei Marichev <prophet314@gmail.com>
2025-02-25 12:49:08 +0500

    Третий этап проекта

---
+++ brain_games/cli.py
@@ -0,0 +1,5 @@
+import prompt
+
+def welcome_user():
+    user_name = prompt.string('May I have your precious name? ')
+    print(f'Hello, {user_name}!')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,9 @@
+from brain_games.cli import welcome_user
+#source .venv/bin/activate
+
 def greet():
     print('Welcome to the Brain Games!')
 
 def main():
     greet()
+    welcome_user()
