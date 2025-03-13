c086d9c07d76cd3a615e757d10c9eb382ad5366c
Sergei Marichev <prophet314@gmail.com>
2025-03-13 12:10:15 +0500

    patched

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,6 +2,6 @@ import prompt
     
 
 def welcome_user():
-    user_name = prompt.string('May I have your precious name? ')
+    user_name = prompt.string('May I have your name? ')
     print(f'Hello, {user_name}!')
     return user_name
